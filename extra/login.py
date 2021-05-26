cf8a9ba0cf3687573daa454e7ef91c3ac81090ba
miaokela <2972799448@qq.com>
2021-05-26 15:12:04 +0800

    DEBUG

--- extra/login.py
+++ extra/login.py
@@ -1,227 +1,270 @@
-# @Time : 2021/5/23 21:37 
-# @Author : xx
-# @File : t2.py.py 
-# @Description: PyCharm
+# @Time : 2021/5/26 9:03 
+# @Author : miaokela
+# @File : TYC_login.py 
+# @Description: 天眼查登录
+
 import random
 import time
-
+from io import BytesIO
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+PHONE = '18868271201'
+PASSWORD = 'laaimeng2011'
+BORDER = 5
+INIT_LEFT = 60
 
 
-class Tyc(object):
-
+class CrackGeetest():
     def __init__(self):
-        Url = 'https://www.tianyancha.com/'
-        self.driver = webdriver.Chrome()
-        self.driver.get(Url)
-        self.driver.maximize_window()
-        self.account = '18868271201'
-        self.password = 'laaimeng2011'
+        self.url = 'https://www.tianyancha.com/'
+        self.browser = webdriver.Chrome()
+        self.wait = WebDriverWait(self.browser, 20)
+        self.browser.get(self.url)
+        self.browser.maximize_window()
+        self.account = PHONE
+        self.password = PASSWORD
 
     def __del__(self):
-        self.driver.close()
+        self.browser.close()
 
-    def show_element(self, element):  # 让验证码图片迅速还原成完整图片
-        self.driver.execute_script("arguments[0].style=arguments[1]", element, "display: block;")
+    def get_geetest_button(self):
+        """
+        获取初始验证按钮
+        :return:
+        """
+        button = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'gt_slider_knob gt_show')))
+        return button
 
-    def hide_element(self, element):  # 暂不知用处
-        self.driver.execute_script("arguments[0].style=arguments[1]", element, "display: none;")
+    def get_position(self):
+        """
+        获取验证码位置
+        :return: 验证码位置元组
+        """
+        img = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'geetest_canvas_img')))
+        time.sleep(2)
+        location = img.location
+        size = img.size
+        top, bottom, left, right = location['y'], location['y'] + size['height'], location['x'], location['x'] + size[
+            'width']
+        return (top, bottom, left, right)
 
-    def open_login(self):
+    def get_screenshot(self):
+        """
+        获取网页截图
+        :return: 截图对象
+        """
+        screenshot = self.browser.get_screenshot_as_png()
+        screenshot = Image.open(BytesIO(screenshot))
+        return screenshot
+
+    def get_slider(self):
+        """
+        获取滑块
+        :return: 滑块对象
+        """
+        slider = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'gt_slider_knob gt_show')))
+        return slider
+
+    def get_geetest_image(self, name='captcha.png'):
+        """
+        获取验证码图片
+        :return: 图片对象
+        """
+        top, bottom, left, right = self.get_position()
+        print('验证码位置', top, bottom, left, right)
+        screenshot = self.get_screenshot()
+        captcha = screenshot.crop((left, top, right, bottom))
+        captcha.save(name)
+        return captcha
+
+    def open(self):
+        """
+        打开网页输入用户名密码
+        :return: None
+        """
         try:
             t = random.uniform(0.5, 1)
             time.sleep(t)
-            login_button = self.driver.find_element_by_xpath('//a[@onclick="header.loginLink(event)"]')
+            login_button = self.browser.find_element_by_xpath('//a[@onclick="header.loginLink(event)"]')
             login_button.click()
             time.sleep(2)
-            pwd_button = self.driver.find_element_by_xpath('//div[@active-tab="1"]')
+            pwd_button = self.browser.find_element_by_xpath('//div[@active-tab="1"]')
             pwd_button.click()
             time.sleep(1)
-            self.driver.find_element_by_xpath('//input[@name="phone"]').send_keys(self.account)
+            self.browser.find_element_by_xpath('//input[@name="phone"]').send_keys(self.account)
             time.sleep(1)
-            self.driver.find_element_by_xpath('//input[@name="password"]').send_keys(self.password)
+            self.browser.find_element_by_xpath('//input[@name="password"]').send_keys(self.password)
             time.sleep(1)
-            click_button = self.driver.find_element_by_xpath(
-                '//div[@class="modal-dialog -login-box animated"]//div[@onclick="loginObj.loginByPhone(event);"]')
+            click_button = self.browser.find_element_by_xpath(
+                '//div[@class="modal-dialog -login-box animated"]//div[@onclick="loginObj.loginByPhone(event);"]'
+            )
             click_button.click()
             time.sleep(2)
 
         except Exception as e:
             print(e)
 
-    def get_image_location(self):
-        # 获取验证码图片路径--->调用get_images截取图片
-        fullbg = self.driver.find_element_by_xpath('//a[@class="gt_fullbg gt_show"]')
-        self.hide_element(fullbg)
-        self.show_element(fullbg)
-        # 获取完整图片
-        self.get_images(fullbg, 'tyc_full.png')
-        # 点击一下滑动按钮触发出来带缺口的图片
-        self.driver.find_element_by_xpath('//div[@class="gt_slider_knob gt_show"]').click()
-        time.sleep(2)
-        # 获取带缺口的图片
-        bg = self.driver.find_element_by_xpath('//div[@class="gt_cut_fullbg gt_show"]')
-        self.show_element(bg)
-        self.get_images(bg, 'tyc_bg.png')
+    def get_gap(self, image1, image2):
+        """
+        获取缺口偏移量
+        :param image1: 不带缺口图片
+        :param image2: 带缺口图片
+        :return:
+        """
+        left = 60
+        for i in range(left, image1.size[0]):
+            for j in range(image1.size[1]):
+                if not self.is_pixel_equal(image1, image2, i, j):
+                    return i
+        return left + 60
+
+    def is_pixel_equal(self, image1, image2, x, y):
+        """
+        判断两个像素是否相同
+        :param image1: 图片1
+        :param image2: 图片2
+        :param x: 位置x
+        :param y: 位置y
+        :return: 像素是否相同
+        """
+        # 取两个图片的像素点
+        pixel1 = image1.load()[x, y]
+        pixel2 = image2.load()[x, y]
+        threshold = 60
+
+        if abs(pixel1[0] - pixel2[0]) > threshold and abs(pixel1[1] - pixel2[1]) > threshold and abs(
+                pixel1[2] - pixel2[2]) > threshold:
+            return False
+        else:
+            return True
+
+    def get_track(self, distance):
+        """
+        根据偏移量获取移动轨迹
+        :param distance: 偏移量
+        :return: 移动轨迹
+        """
+        # 移动轨迹
+        track = []
+        # 当前位移
+        current = 0
+        # 减速阈值
+        mid = distance * 4 / 5
+        # 计算间隔
+        t = 0.2
+        # 初速度
+        v = 0
+
+        while current < distance:
+            if current < mid:
+                a = 2
+            else:
+                a = -2
+            # 初速度v0
+            v0 = v
+            # 当前速度v = v0 + at
+            v = v0 + a * t
+            # 移动距离x = v0t + 1/2 * a * t^2
+            move = v0 * t + 1 / 2 * a * t * t
+            # 当前位移
+            current += move
+            # 加入轨迹
+            track.append(round(move))
+        track += [2, -2]
+        return track
+
+    def move_to_gap(self, trace):
+        t = random.uniform(0, 0.5)
+        # 得到滑块标签
+        slider = self.browser.find_element_by_xpath('//div[@class="gt_slider_knob gt_show"]')
+        # 使用click_and_hold()方法悬停在滑块上，perform()方法用于执行
+        ActionChains(self.browser).click_and_hold(slider).perform()
+        for x in trace:
+            # 使用move_by_offset()方法拖动滑块，perform()方法用于执行
+            ActionChains(self.browser).move_by_offset(xoffset=x, yoffset=0).perform()
+        # 模拟人类对准时间
+        time.sleep(t)
+        # 释放滑块
+        ActionChains(self.browser).release().perform()
+
+    def login(self):
+        """
+        登录
+        :return: None
+        """
+        submit = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'login-btn')))
+        submit.click()
+        time.sleep(10)
+        print('登录成功')
+
+    def show_element(self, element):  # 让验证码图片迅速还原成完整图片
+        self.browser.execute_script("arguments[0].style=arguments[1]", element, "display: block;")
+
+    def hide_element(self, element):  # 暂不知用处
+        self.browser.execute_script("arguments[0].style=arguments[1]", element, "display: none;")
 
     def get_images(self, img, name):
         # 截图整个网页
-        self.driver.save_screenshot(r'./Images/Tyc.png')
+        self.browser.save_screenshot(r'./Images/Tyc.png')
         # 验证码路径
         location = img.location
         # 验证码尺寸
         size = img.size
+
         # top, bottom, left, right = location['y']+3, location['y'] + size['height'], location['x']-325, location['x'] + size['width']-328
         # 加入代码self.driver.maximize_window()使浏览器全屏后就不需要修改位置参数了,mac上不知道什么原因需要修改位置
-        top, bottom, left, right = location['y'], location['y'] + size['height'], location['x'], location['x'] + size[
-            'width']
+        top, bottom, left, right = location['y'], location['y'] + size['height'] - 20, location['x'], location['x'] + \
+                                   size['width']
         picture = Image.open(r'./Images/Tyc.png')
         picture = picture.crop((left, top, right, bottom))
         picture.save(r'./Images/%s' % name)
         time.sleep(0.5)
 
-    def is_pixel_equal(self, tyc_bg, tyc_full, x, y):
-        """
-        :param tyc_bg: 带缺口图片
-        :param tyc_full: 完整图片
-        :param x: (Int) x位置
-        :param y: (Int) y位置
-        :return: (Boolean) 像素是否相同
-        """
-        # 获取缺口图片的像素点（按RGE格式）
-        bg_pixel = tyc_bg.load()[x, y]
-        # 获取完整图片的像素点（按RGB格式）
-        full_pixel = tyc_full.load()[x, y]
-        threshold = 60
-        if (abs(full_pixel[0] - bg_pixel[0] < threshold) and abs(full_pixel[1] - bg_pixel[1] < threshold) and abs(
-                full_pixel[2] - bg_pixel[2] < threshold)):
-            # 如果差值在判断值之内，返回是相同像素
-            return True
-
-    def get_distance(self, image1, image2):
-        """
-        拿到滑动验证码需要移动的距离
-        :param image_name1:没有缺口的图片对象
-        :param image_name2:带缺口的图片对象
-        :return:需要移动的距离
-        """
-        threshold = 60
-        left = 60
-        for i in range(left, image1.size[0]):
-            for j in range(image1.size[1]):
-                rgb1 = image1.load()[i, j]
-                rgb2 = image2.load()[i, j]
-                res1 = abs(rgb1[0] - rgb2[0])
-                res2 = abs(rgb1[1] - rgb2[1])
-                res3 = abs(rgb1[2] - rgb2[2])
-                if not (res1 < threshold and res2 < threshold and res3 < threshold):
-                    return i
-
-    # def get_distance(self, tyc_bg, tyc_full):
-    #     """
-    #     :param tyc_bg: 带缺口图片
-    #     :param tyc_full: 完整图片
-    #     :return: (Int) 滑块与缺口的距离
-    #     """
-    #     # 滑块位置初始设置为 60 (这个是去掉开始的一部分)
-    #     left_dist = 60
-    #     # 遍历像素点横坐标
-    #     for i in range(left_dist, tyc_full.size[0]):
-    #         # 遍历像素点纵坐标
-    #         for j in range(tyc_full.size[1]):
-    #             if not self.is_pixel_equal(tyc_bg, tyc_full, i, j):
-    #                 # 返回此时横轴坐标就是滑块需要移动的距离
-    #                 return i
-
-    def get_trace(self, distance):
-        """
-        拿到移动轨迹，模仿人的滑动行为，先匀加速后匀减速
-        匀变速运动基本公式：
-        ①v=v0+at
-        ②s=v0t+(1/2)at²
-        ③v²-v0²=2as
-
-        :param distance: 需要移动的距离
-        :return: 存放每0.2秒移动的距离
-        """
-        # 初速度
-        v = 0
-        # 单位时间为0.2s来统计轨迹，轨迹即0.2内的位移
-        t = 0.1
-        # 位移/轨迹列表，列表内的一个元素代表0.2s的位移
-        tracks = []
-        # 当前的位移
-        current = 0
-        # 到达mid值开始减速
-        mid = distance * 4 / 5
-
-        distance += 10  # 先滑过一点，最后再反着滑动回来
-
-        while current < distance:
-            if current < mid:
-                # 加速度越小，单位时间的位移越小,模拟的轨迹就越多越详细
-                a = 2  # 加速运动
-            else:
-                a = -3  # 减速运动
-
-            # 初速度
-            v0 = v
-            # 0.2秒时间内的位移
-            s = v0 * t + 0.5 * a * (t ** 2)
-            # 当前的位置
-            current += s
-            # 添加到轨迹列表
-            tracks.append(round(s))
-
-            # 速度已经达到v,该速度作为下次的初速度
-            v = v0 + a * t
-
-        # 反着滑动到大概准确位置
-        for i in range(3):
-            tracks.append(-2)
-        for i in range(4):
-            tracks.append(-1)
-        return tracks
-
-    def move_to_gap(self, trace):
-        t = random.uniform(0, 0.5)
-        # 得到滑块标签
-        slider = self.driver.find_element_by_xpath('//div[@class="gt_slider_knob gt_show"]')
-        # 使用click_and_hold()方法悬停在滑块上，perform()方法用于执行
-        ActionChains(self.driver).click_and_hold(slider).perform()
-        for x in trace:
-            # 使用move_by_offset()方法拖动滑块，perform()方法用于执行
-            ActionChains(self.driver).move_by_offset(xoffset=x, yoffset=0).perform()
-        # 模拟人类对准时间
-        time.sleep(t)
-        # 释放滑块
-        ActionChains(self.driver).release().perform()
+    def get_image_location(self):
+        # 获取验证码图片路径--->调用get_images截取图片
+        fullbg = self.browser.find_element_by_xpath('//a[@class="gt_fullbg gt_show"]')
+        self.hide_element(fullbg)
+        self.show_element(fullbg)
+        # 获取完整图片
+        self.get_images(fullbg, 'captcha1.png')
+        # 点击一下滑动按钮触发出来带缺口的图片
+        self.browser.find_element_by_xpath('//div[@class="gt_slider_knob gt_show"]').click()
+        time.sleep(2)
+        # 获取带缺口的图片
+        bg = self.browser.find_element_by_xpath('//div[@class="gt_cut_fullbg gt_show"]')
+        self.show_element(bg)
+        self.get_images(bg, 'captcha2.png')
 
     def slice(self):
-        bg_image = Image.open(r'./Images/tyc_bg.png')
-        full_image = Image.open(r'./Images/tyc_full.png')
         try:
-            # 计算位置距离
-            distance = self.get_distance(bg_image, full_image)
-            print('计算偏移量为：%s Px' % distance)
-            # 位移算法
-            trace = self.get_trace(int(distance))  # 拖动过来可以上来先做减值
-            print(trace)
-            # 移动滑块
-            self.move_to_gap(trace)
+            image2 = Image.open(r'./Images/captcha2.png')
+            image1 = Image.open(r'./Images/captcha1.png')
+            # 获取缺口位置
+            gap = self.get_gap(image1, image2)
+            print('缺口位置', gap)
+            # 减去缺口位移
+            gap -= BORDER
+            # 获取移动轨迹
+            track = self.get_track(gap)
+            # print('滑动轨迹', track)
+            # 拖动滑块
+            self.move_to_gap(track)
             time.sleep(0.5)
             while True:
-                for i in range(15, 20):
+                for i in range(8, 20):
                     try:
-                        mspan = self.driver.find_element_by_xpath('//div[@class="gt_info_text"]/span[2]').text
+                        mspan = self.browser.find_element_by_xpath('//div[@class="gt_info_text"]/span[2]').text
                         print(mspan)
                         if '拖动滑块' in mspan:
                             time.sleep(1)
-                            distance = self.get_distance(bg_image, full_image)
+                            distance = self.get_gap(image1, image2)
                             print('计算偏移量为：%s Px' % distance)
-                            trace = self.get_trace(int(distance) + int(i))
+                            trace = self.get_track(int(distance) - int(i))
                             print('减值%s' % i)
                             print(trace)
                             # 移动滑块
@@ -246,39 +289,13 @@ class Tyc(object):
             else:
                 print(e)
 
-    def get_cookie(self):
-        time.sleep(3)
-        try:
-            login_name = self.driver.find_element_by_xpath('//div[@nav-type="user"]/a').get_attribute("text")
-            print(login_name)
-            time.sleep(3)
-            cookie = {}
-            cookies = self.driver.get_cookies()
-            for i in cookies:
-                a = i['name']
-                b = i['value']
-                cookie[a] = b
-            self.cookie_to_redis(cookie)
-        except Exception as e:
-            print(e)
-
-    def cookie_to_redis(self, cookie):
-        # 连接数据库
-        from redis import StrictRedis, ConnectionPool
-        # 使用连接池
-        pool = ConnectionPool(host='localhost', port=6379, db=0)
-        rds = StrictRedis(connection_pool=pool)
-        rds.set('ck', '%s' % cookie)
-        # 然后将其获取并打印
-        print(rds.get('ck'))
-
-    def entrace(self):
-        self.open_login()
+    def crack(self):
+        self.open()
         self.get_image_location()
         self.slice()
-        self.get_cookie()
+        time.sleep(5)
 
 
 if __name__ == '__main__':
-    tyc = Tyc()
-    tyc.entrace()
+    crack = CrackGeetest()
+    crack.crack()
