ae87c3b825d97cf79194cc9c3306e161a97a4022
miaokela <2972799448@qq.com>
2021-08-05 21:44:51 +0800

    - DEBUG

--- spider_pro/middlewares/ProxyMiddleware.py
+++ spider_pro/middlewares/ProxyMiddleware.py
@@ -256,19 +256,19 @@ class ProxyMiddleware(RetryMiddleware):
         q_count = 0
         c_count = 0
         while True:
-            try:
-                self.logger.info('强制终止计数(至180)，当前：{0}'.format(c_count))
-                if c_count == 180:  # 10s 循环一次 半小时请求数不变表示阻塞 强制终止爬虫
-                    self.logger.info('正在强制终止爬虫...')
-                    crawler.engine.close_spider(self, '长时间阻塞，强制终止爬虫.')
-                    exit(0)
-                if q_count == len(crawler.engine.slot.scheduler):
-                    c_count += 1
-                else:
-                    c_count = 0
-                q_count = len(crawler.engine.slot.scheduler)
-            except Exception as e:
-                print('catch error:{0}'.format(e))
+            # try:
+            #     self.logger.info('强制终止计数(至180)，当前：{0}'.format(c_count))
+            #     if c_count == 180:  # 10s 循环一次 半小时请求数不变表示阻塞 强制终止爬虫
+            #         self.logger.info('正在强制终止爬虫...')
+            #         crawler.engine.close_spider(self, '长时间阻塞，强制终止爬虫.')
+            #         exit(0)
+            #     if q_count == len(crawler.engine.slot.scheduler):
+            #         c_count += 1
+            #     else:
+            #         c_count = 0
+            #     q_count = len(crawler.engine.slot.scheduler)
+            # except Exception as e:
+            #     print('catch error:{0}'.format(e))
             # 更新redis ip池
             for name_proxy in [self.name_http_proxy, self.name_https_proxy]:
                 max_num = self.max_http if name_proxy == self.name_http_proxy else self.max_https
