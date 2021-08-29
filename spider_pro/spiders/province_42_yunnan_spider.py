577bb417e6438c3ec4d24b6ae511b17dc98150b8
miaokela <2972799448@qq.com>
2021-08-29 22:51:38 +0800

    - ADD 云南省公共资源中心

--- spider_pro/spiders/province_42_yunnan_spider.py
+++ spider_pro/spiders/province_42_yunnan_spider.py
@@ -1,6 +1,6 @@
 # author: miaokela
 # Date: 2021-04-14 16:24:06
-# Description: 云南
+# Description: 云南省公共资源中心
 from lxml import etree
 import scrapy
 import re
@@ -31,18 +31,15 @@ class Province42YunnanSpiderSpider(scrapy.Spider):
         },
         '资格预审结果公告': {
             'url': [],
-            # 'keywords': ['资格预审'],
         },
         '招标变更': {
             'url': [
                 'http://ggzy.yn.gov.cn/jyxx/jsgcBgtz?area=000&secondArea=',
                 'http://ggzy.yn.gov.cn/jyxx/zfcg/gzsx?area=000&secondArea=',
             ],
-            # 'keywords': ['中止', '终止', '异常', '废标', '流标'],
         },
         '中标预告': {
             'url': ['http://ggzy.yn.gov.cn/jyxx/jsgcpbjggs?area=000&secondArea='],
-            # 'keywords': ['候选人'],
         },
         '中标公告': {
             'url': [
@@ -61,9 +58,8 @@ class Province42YunnanSpiderSpider(scrapy.Spider):
             'keywords': [],
         }
     }
-
     area_id = 42
-    basic_area = '云南'
+    basic_area = '云南省公共资源中心'
 
     def __init__(self, *args, **kwargs):
         super().__init__()
@@ -85,57 +81,55 @@ class Province42YunnanSpiderSpider(scrapy.Spider):
                     'notice_type': notice_type
                 })
 
-    def parse_urls(self, response):
+    def parse_urls(self, resp):
         """
         get max pages
         """
-        total_page = response.xpath('//input[@name="totalPage"]/@value').get()
+        total_page = resp.xpath('//input[@name="totalPage"]/@value').get()
+        headers = utils.get_headers(resp)
+        proxies = utils.get_proxies(resp)
         try:
             total_page = int(total_page)
         except ValueError as e:
             total_page = 1
             self.log(e)
 
-        for p in range(1, 2):
-            # for p in range(1, total_page + 1):
-            yield scrapy.FormRequest(url=response.url, formdata={'currentPage': str(p)}, callback=self.parse_data_urls,
-                                     meta={
-                                         'notice_type': response.meta.get('notice_type')
-                                     })
+        if all([self.start_time, self.end_time]):
+            for p in range(1, total_page + 1):
+                judge_status = utils.judge_in_interval(
+                    resp.url, start_time=self.start_time, end_time=self.end_time, method='POST',
+                    data={'currentPage': str(p)}, proxies=proxies, headers=headers,
+                    rule='//table[@id="data_tab"]/tbody/tr[position()>1]/td[4]/text()'
+                )
+                if judge_status == 0:
+                    break
+                elif judge_status == 2:
+                    continue
+                else:
+                    yield scrapy.FormRequest(
+                        url=resp.url, formdata={'currentPage': str(p)}, callback=self.parse_data_urls, meta={
+                            'notice_type': resp.meta.get('notice_type')
+                        }, dont_filter=True, priority=total_page - p)
+        else:
+            for p in range(1, total_page + 1):
+                yield scrapy.FormRequest(
+                    url=resp.url, formdata={'currentPage': str(p)}, callback=self.parse_data_urls, meta={
+                        'notice_type': resp.meta.get('notice_type')
+                    }, dont_filter=True, priority=total_page - p)
 
-    def parse_data_urls(self, response):
-        # find pub_time index
-        pub_time_positions = response.xpath('//*[@id="data_tab"]/tbody/tr[position()=1]/th')
-        pub_time_index = 1
-        title_index = 1
-        for n, pub_time_position in enumerate(pub_time_positions):
-            title = pub_time_position.xpath('./text()').get()
-            if title == '发布时间':
-                pub_time_index = n + 1
-                break
-        for n, pub_time_position in enumerate(pub_time_positions):
-            title = pub_time_position.xpath('./text()').get()
-            if title in ['公告标题', '变更标题', '公告名称', '采购项目名称', '公示标题', '项目名称', '矿山/项目名称']:
-                title_index = n + 1
-                break
-        els = response.xpath('//*[@id="data_tab"]/tbody/tr[position()>1]')
-        title_com = re.compile('\[(.*?)\](.*)')
+    def parse_data_urls(self, resp):
+        els = resp.xpath('//table[@id="data_tab"]/tbody/tr[position()>1]')
+        title_com = re.compile(r'\[(.*?)\](.*)')
 
-        for el in els:
+        for n, el in enumerate(els):
             detail_url = el.xpath('.//a/@href').get()
-            pub_time = el.xpath('./td[position()={0}]/text()'.format(pub_time_index)).get().strip()
+            pub_time = el.xpath('./td[4]/text()').get().strip()
 
-            try:
-                pub_time = int(pub_time)
-            except ValueError:
-                self.log(pub_time_index)
-
-            art_title_ori = el.xpath('./td[position()={0}]/a/text()'.format(title_index)).get()
-
-            art_title = re.sub('\s|\t|\n', '', art_title_ori if art_title_ori else '')
+            art_title_ori = el.xpath('./td[3]/a/text()').get()
+            art_title = re.sub(r'\s|\t|\n', '', art_title_ori if art_title_ori else '')
 
             # 处理pub_time，判断在start_time与end_time区间内
-            if utils.check_range_time(self.start_time, self.end_time, pub_time):
+            if utils.check_range_time(self.start_time, self.end_time, pub_time)[0]:
                 art_titles = title_com.findall(art_title)
                 title_split = art_titles[0] if art_titles else [art_title]
 
@@ -148,10 +142,10 @@ class Province42YunnanSpiderSpider(scrapy.Spider):
                     info_source = self.basic_area
 
                 yield scrapy.Request(url=self.query_url + detail_url, callback=self.parse_item, meta={
-                    'notice_type': response.meta.get('notice_type'),
+                    'notice_type': resp.meta.get('notice_type'),
                     'pub_time': pub_time,
                     'info_source': info_source.strip(),
-                })
+                }, dont_filter=True, priority=10000 * (len(els) - n))
 
     def match_title(self, title_name):
         """
@@ -171,36 +165,33 @@ class Province42YunnanSpiderSpider(scrapy.Spider):
                 break
         return matched, notice_type
 
-    def parse_item(self, response):
-        content = response.xpath('//div[contains(@class, "page_contect")]').get()
-        # title_name包含省略号 需要从文章获取
-        title_name = response.xpath('//h3[@class="detail_t"]/text()').get()
-
-        # 内容防止转义字符
-        content = utils.avoid_escape(content)
+    def parse_item(self, resp):
+        content = resp.xpath('//div[@class="detail_contect"]').get()
+        title_name = resp.xpath('//h3[@class="detail_t"]/text()').get()
+        pub_time = resp.meta.get('pub_time')
         # 去除温馨提示
         _, content = utils.remove_specific_element(content, 'div', 'class', 'receipt_box')
         # 投标文件
-        _, files_path = utils.catch_files(content, self.query_url)
+        _, files_path = utils.catch_files(content, self.query_url, pub_time=pub_time, resp=resp)
 
-        category = response.xpath('/html/body/div[4]/div/a[3]/text()').get()
-        notice_type_ori = response.meta.get('notice_type')
+        category = resp.xpath('/html/body/div[4]/div/a[3]/text()').get()
+        notice_type_ori = resp.meta.get('notice_type')
 
-        # title_name = response.meta.get('title_name')
         # 关键词匹配 修改notice_type
         matched, match_notice_type = self.match_title(title_name)
         if matched:
             notice_type_ori = match_notice_type
 
         notice_types = list(
-            filter(lambda k: constans.TYPE_NOTICE_DICT[k] == notice_type_ori, constans.TYPE_NOTICE_DICT))
+            filter(lambda k: constans.TYPE_NOTICE_DICT[k] == notice_type_ori, constans.TYPE_NOTICE_DICT)
+        )
 
         notice_item = items.NoticesItem()
-        notice_item["origin"] = response.url
+        notice_item["origin"] = resp.url
         notice_item["title_name"] = title_name
-        notice_item["pub_time"] = response.meta.get('pub_time')
+        notice_item["pub_time"] = pub_time
 
-        notice_item["info_source"] = response.meta.get('info_source')
+        notice_item["info_source"] = resp.meta.get('info_source')
         notice_item["is_have_file"] = constans.TYPE_HAVE_FILE if files_path else constans.TYPE_NOT_HAVE_FILE
         notice_item["files_path"] = files_path
         notice_item["notice_type"] = notice_types[0] if notice_types else constans.TYPE_UNKNOWN_NOTICE
@@ -208,12 +199,12 @@ class Province42YunnanSpiderSpider(scrapy.Spider):
         notice_item["area_id"] = self.area_id
         notice_item["category"] = category
 
-        print('yield to pipeline')
+        print(resp.meta.get('pub_time'), resp.url)
         return notice_item
 
 
 if __name__ == "__main__":
     from scrapy import cmdline
 
-    # cmdline.execute("scrapy crawl province_42_yunnan_spider -a sdt=2021-04-10 -a edt=2021-04-14".split(" "))
+    # cmdline.execute("scrapy crawl province_42_yunnan_spider -a sdt=2021-08-10 -a edt=2021-08-29".split(" "))
     cmdline.execute("scrapy crawl province_42_yunnan_spider".split(" "))
