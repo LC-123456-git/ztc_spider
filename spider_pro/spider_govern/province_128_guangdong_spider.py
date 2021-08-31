73afa6e2bf078bd3c9985b065e70408f48bba82d
miaokela <2972799448@qq.com>
2021-08-31 09:45:28 +0800

    - CHANGE

--- spider_pro/spider_govern/province_128_guangdong_spider.py
+++ spider_pro/spider_govern/province_128_guangdong_spider.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @file           :province_128_guangdong_spider.py
-# @description    :黑龙江省政府采购网
+# @description    :广东省政府采购网
 # @date           :2021/08/24
 # @author         :miaokela
 # @version        :1.0
