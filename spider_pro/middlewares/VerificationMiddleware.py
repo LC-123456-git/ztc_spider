f349c1cf350d09b7eb89472a72100a3aca6a7dc9
miaokela <2972799448@qq.com>
2021-09-01 11:35:28 +0800

    - ADD

--- spider_pro/middlewares/VerificationMiddleware.py
+++ spider_pro/middlewares/VerificationMiddleware.py
@@ -10,6 +10,8 @@ import requests
 from PIL import Image
 from pytesseract import *
 from scrapy.downloadermiddlewares.retry import RetryMiddleware
+import random
+import re
 
 class VerificationMiddleware(RetryMiddleware):
 
@@ -22,6 +24,7 @@ class VerificationMiddleware(RetryMiddleware):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
         }
         self.settings = kwargs
+        self.tag = 0
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -34,24 +37,36 @@ class VerificationMiddleware(RetryMiddleware):
         img_path = os.path.join(self.settings.get('IMAGES_PATH'), 'images.jpeg')
         with open(img_path, 'wb') as fp:
             fp.write(img_content.content)
-        img_code = image_to_string(Image.open(img_path), lang='eng', config='-psm 10').strip()
+        img = Image.open(img_path)
+        txt = pytesseract.image_to_string(img)
+        txt = ''.join(txt.split(' '))
+        com = re.compile(r'(\d+)')
+        img_code = com.findall(txt)
+        # img_code = image_to_string(Image.open(img_path), lang='eng', config='-psm 10').strip()
         return img_code
 
-    def cookie_to_dic(self, response):
-        new_dict = response.meta['new_dict'] | {'authCode': self.get_auth_code()}
+    def cookie_to_dic(self, request):
+        new_dict = request.meta['new_dict'] | {'authCode': self.get_auth_code()}
         resp = requests.post(url=self.url, data=new_dict, headers=self.headers)
         cookie_dict = requests.utils.dict_from_cookiejar(resp.cookies)
-        _cookies = 'JSESSIONID' + '=' + cookie_dict['JSESSIONID']
-        return _cookies
+        # _cookies = 'JSESSIONID' + '=' + cookie_dict['JSESSIONID']
+        return cookie_dict
 
     def process_response(self, request, response, spider):
         try:
-            new_dict = response.meta['new_dict']
-            respon = requests.post(url=response.url, data=new_dict, headers=response.headers)
-            if respon.text == 'reload':
-                request.headers = response.headers | {'cookie': self.cookie_to_dic(response)}
+            if response.text == 'reload':
+                self.tag += 1
+                # new_dict = request.meta['new_dict']
+                # headers = {k: random.choice(v) if all([isinstance(v, list), v]) else v for k, v in response.headers.items()}
+                # respon = requests.post(url=response.url, data=new_dict, headers=headers)
+                # if respon.text == 'reload':
+                # request.headers = response.headers | {'cookie': self.cookie_to_dic(request)}
+                if self.tag < 2:
+                    request.cookies = self.cookie_to_dic(request)
+                
                 # respon = self.process_response(request, response, spider)
-            return self._retry(request, 'nima', spider) or response
+                return self._retry(request, 'nima', spider)
+            return response
         except Exception as e:
             return response
 
