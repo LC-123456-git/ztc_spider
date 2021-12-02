5dba428a2299e04f3d147102b6aa488c4ecd6207
liuchao <842008305@qq.com>
2021-12-02 09:00:16 +0800

    add 全角转半角

--- spider_pro/rules_clean.py
+++ spider_pro/rules_clean.py
@@ -5,6 +5,8 @@ import re
 import cn2an
 import pandas
 import copy
+
+from idna import unichr
 from lxml import etree
 from decimal import Decimal
 from html_table_extractor.extractor import Extractor
@@ -146,6 +148,20 @@ class KeywordsExtract(object):
             self.fields_regular.update(**{
                 self.field_name: self.common_regulars
             })
+
+    @staticmethod
+    def strQ2B(ustring):
+        """全角转半角"""
+        rstring = ""
+        for uchar in ustring:
+            inside_code = ord(uchar)
+            if inside_code == 12288:  # 全角空格直接转换
+                inside_code = 32
+            elif (inside_code >= 65281 and inside_code <= 65374):  # 全角字符（除空格）根据关系转化
+                inside_code -= 65248
+
+            rstring += unichr(inside_code)
+        return rstring
 
     def reload_multi_number(self, key):
         """
@@ -553,7 +569,8 @@ class KeywordsExtract(object):
         # 多空格转化成单个
         self._value = KeywordsExtract.remove_chars_by_regs([r'\s+'], self._value, replace_str=' ')
         if self.field_name == 'tenderopen_time':
-            self._value = KeywordsExtract.format_tenderopen_time(self._value)
+            _value = KeywordsExtract.format_tenderopen_time(self._value)
+            self._value = KeywordsExtract.strQ2B(_value)
         if self.field_name in ['tenderee', 'bidding_agency', 'successful_bidder']:
             self._value = KeywordsExtract.remove_chars_by_regs(self.company_regulars, self._value)
         if self.field_name in ['bid_amount', 'budget_amount']:
