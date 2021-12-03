79c7450453746a6d5b3c1dc79e670f3fe9940156
liuchao <842008305@qq.com>
2021-12-03 16:30:59 +0800

    add

--- spider_pro/utils.py
+++ spider_pro/utils.py
@@ -29,7 +29,6 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'
 }
 
-
 def get_crwal_turn_page_command(pub_time, start_time, end_time):
     """
     + 不在区间内不采集
