74df3ba27b52a9a8d7b63e7acb015573813e619e
miaokela <2972799448@qq.com>
2021-12-08 11:26:49 +0800

    - 更新青岛政府爬虫

--- spider_pro/spider_govern/province_145_qingdao_spider.py
+++ spider_pro/spider_govern/province_145_qingdao_spider.py
@@ -26,6 +26,7 @@ class Province145QingdaoSpiderSpider(scrapy.Spider):
     basic_area = '青岛市政府采购网'
     query_url = 'http://www.ccgp-qingdao.gov.cn/sdgp2014/dwr/call/plaincall/dwrmng.queryWithoutUi.dwr'
     base_url = 'http://www.ccgp-qingdao.gov.cn'
+    notice_base_url = 'http://www.ccgp-qingdao.gov.cn/sdgp2014/site/'
     first_url = 'http://www.ccgp-qingdao.gov.cn/sdgp2014/site/channelall370200.jsp?colcode=0401&flag=0401'
     detail_url = 'http://www.ccgp-qingdao.gov.cn/sdgp2014/site/read370200.jsp?id={id}&flag=0401'
 
@@ -310,12 +311,74 @@ class Province145QingdaoSpiderSpider(scrapy.Spider):
                 })
                 if utils.check_range_time(self.start_time, self.end_time, pub_time)[0]:
                     yield scrapy.Request(
-                        url=c_url, callback=self.parse_detail,
+                        url=c_url, callback=self.catch_detail_url,
                         meta=resp.meta, priority=(len(rslt_string_list) - n) * 10 ** 4
                     )
 
+    def catch_detail_url(self, resp):
+        """
+        - 获取【显示公告正文】链接
+        :param resp:
+        :return:
+        """
+        inner_els = resp.xpath('//div[@class="biaotq"]/a[text()="【显示公告正文】"]')
+
+        if inner_els:
+            suffix_params_com = re.compile(r'var\s*url1\s*=\s*\"(.*?)\";.*?msgWindow')
+            suffix_params = suffix_params_com.findall(resp.text.replace('\n', ''))
+
+            if suffix_params:
+                suffix_params = suffix_params[0]
+                yield scrapy.Request(
+                    url=''.join([self.notice_base_url, suffix_params]), callback=self.parse_detail,
+                    meta=resp.meta, priority=10 ** 10
+                )
+            else:
+                self.logger.info(f'请求{resp.url}时没有获取到【显示公告正文】链接.')
+        else:
+            content = resp.xpath('//div[contains(@style, "overflow-x:auto; width:100%;")]').get()
+            title_name = resp.meta.get('title')
+            pub_time = resp.meta.get('pub_time')
+            notice_type_ori = resp.meta.get('notice_type')
+
+            matched, match_notice_type = self.match_title(title_name)
+            if matched:
+                notice_type_ori = match_notice_type
+
+            notice_types = list(
+                filter(lambda k: constans.TYPE_NOTICE_DICT[k] == notice_type_ori, constans.TYPE_NOTICE_DICT)
+            )
+
+            # 移除相关信息
+            _, content = utils.remove_element_by_xpath(
+                content,
+                xpath_rule='//table[.//img[@src="images/huan.jpg"]]'
+            )
+            _, content = utils.remove_element_by_xpath(
+                content,
+                xpath_rule='//div[@style="text-align:center;"]'
+            )
+            # 匹配文件
+            _, files_path = utils.catch_files(content, self.base_url, pub_time=pub_time, resp=resp)
+
+            notice_item = items.NoticesItem()
+            notice_item["origin"] = resp.url
+
+            notice_item["title_name"] = title_name.strip() if title_name else ''
+            notice_item["pub_time"] = pub_time
+
+            notice_item["info_source"] = self.basic_area
+            notice_item["is_have_file"] = constans.TYPE_HAVE_FILE if files_path else constans.TYPE_NOT_HAVE_FILE
+            notice_item["files_path"] = files_path
+            notice_item["notice_type"] = notice_types[0] if notice_types else constans.TYPE_UNKNOWN_NOTICE
+            notice_item["content"] = content
+            notice_item["area_id"] = self.area_id
+            notice_item["category"] = '政府采购'
+            print(resp.meta.get('pub_time'), resp.url)
+            return notice_item
+
     def parse_detail(self, resp):
-        content = resp.xpath('//div[contains(@style, "overflow-x:auto; width:100%;")]').get()
+        content = resp.xpath('//div[@class="cont"]').get()
         title_name = resp.meta.get('title')
         pub_time = resp.meta.get('pub_time')
         notice_type_ori = resp.meta.get('notice_type')
@@ -331,11 +394,11 @@ class Province145QingdaoSpiderSpider(scrapy.Spider):
         # 移除相关信息
         _, content = utils.remove_element_by_xpath(
             content,
-            xpath_rule='//table[.//img[@src="images/huan.jpg"]]'
+            xpath_rule='//div[contains(@style, "windowtext")]'
         )
         _, content = utils.remove_element_by_xpath(
             content,
-            xpath_rule='//div[@style="text-align:center;"]'
+            xpath_rule='//div[contains(@style, "text-align:center")]'
         )
         # 匹配文件
         _, files_path = utils.catch_files(content, self.base_url, pub_time=pub_time, resp=resp)
